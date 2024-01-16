d7eb07c0f00d4eb842526714b89881b420ef9070
Darjetii-Dooku <7220339@gmail.com>
2024-01-16 20:17:27 +0200

    friends last

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,12 +5,12 @@ const FriendList = ({friends}) => {
     console.log(friends);
   return (
     <div>
-        <ul class="friend-list">
+        <ul className={css.list}>
             {friends.map(friend => 
                 <li key={friend.id} className={css.item}>
                 <span className={friend.isOnline ? css.online : css.ofline}></span>
-                <img className={css.avatar} src="" alt="User avatar" width="48" />
-                <p className={css.friend_name}></p>
+                <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
+                <p className={css.friend_name}>{friend.name}</p>
               </li>
             )}
         </ul>
